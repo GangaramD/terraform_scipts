e62ea2e2e3d142c77ac8a669deb3e0998d3d2d76
GangaramD <gangaram136@gmail.com>
2018-12-09 15:10:26 +0530

    Update droplet.tf

--- droplet.tf
+++ droplet.tf
@@ -1,8 +1,8 @@
 resource "digitalocean_droplet" "web" {
     image = "ubuntu-16-04-x64"
-    name = "mytest"
+    name = "nexus"
     region = "blr1"
-    size = "s-1vcpu-2gb"
+    size = "s-2vcpu-4gb"
     private_networking = true
     ssh_keys = [
       "69:f6:aa:f6:a2:94:58:f2:41:b0:fb:69:09:e6:87:dd"
