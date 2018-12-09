2c7027fac17d281eaa38bd84706d936ebba5c55c
GangaramD <gangaram136@gmail.com>
2018-12-09 17:12:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,7 @@
 variable "do_token" { }
 variable "pub_key" { }
 variable "pvt_key" { }
-variable "ssh_fingerprint" { 
-                     default = "69:f6:aa:f6:a2:94:58:f2:41:b0:fb:69:09:e6:87:dd"
-}
+variable "ssh_fingerprint" { }
 provider "digitalocean" {
   token = "${var.do_token}"
 }
